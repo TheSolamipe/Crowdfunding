b5b3520397af504d179782428e8439f1b16f0caa
Solamipe Akinola <thesolamipe@Solamipes-MacBook-Pro.local>
2023-09-12 11:03:26 +0100

    Contract connected with frontend and we can create campaign

---
+++ client/src/context/index.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, createContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0xD6B7469665Aeb40709B064f14874a31d9E4Eb865"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+  const amountCollected = 0;
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign({
+				args: [
+					address, // owner
+					form.title, // title
+					form.description, // description
+					form.target,
+					new Date(form.deadline).getTime(), // deadline,
+          amountCollected,
+					form.image,
+				],
+			});
+
+      console.log("contract call success", data);
+    } catch (error) {
+      console.log("contract call failure", error);
+    }
+  };
+
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
